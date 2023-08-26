1959ba1b99d8c3abc34e1b6815302541c1c2d5e1
io42630 <ivan@olexyn.com>
2023-08-26 17:31:33 +0200

    add more Constants

--- src/main/java/com/olexyn/tabdriver/Constants.java
+++ src/main/java/com/olexyn/tabdriver/Constants.java
@@ -3,4 +3,9 @@ package com.olexyn.tabdriver;
 public interface Constants {
 
 	String EMPTY = "";
+	String BUTTON = "button";
+	String INPUT = "input";
+	String ID = "id";
+	String TABLE = "table";
+	String DIV = "div";
 }
